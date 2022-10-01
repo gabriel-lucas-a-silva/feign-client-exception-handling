eb8e22600ec8c1e2d89c8ddd1a9cb06ca93cdd4c
Gabriel Lucas <gabriellcontatoprofissional@gmail.com>
2022-10-01 18:12:45 -0300

    adding base project

---
+++ FeignClientExceptionHandling/src/main/java/com/codemore/FeignClientExceptionHandling/FeignClientExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.codemore.FeignClientExceptionHandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientExceptionHandlingApplication.class, args);
+	}
+
+}
